e538afa2c308c3c5e0827ff438bb0d3b235932e1
Martin Schröter <martin.schroeter@namics.com>
2016-12-17 13:30:23 +0100

    added another demo project

---
+++ demo/Main.elm
@@ -0,0 +1,65 @@
+module Main exposing (..)
+
+import Html exposing (Html, div)
+
+
+-- MAIN
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    {}
+
+
+type alias Msg =
+    {}
+
+
+model : Model
+model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( model, Cmd.none )
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [] []
